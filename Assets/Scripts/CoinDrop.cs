783adcc6a366c1993057e3c719827bfa1a164957
Balintsz0800 <balint0800@gmail.com>
2026-01-31 10:40:20 +0100

    Added weapon selector

--- Assets/Scripts/CoinDrop.cs
+++ Assets/Scripts/CoinDrop.cs
@@ -5,13 +5,20 @@ using Random = UnityEngine.Random;
 public class CoinDrop : MonoBehaviour
 {
     [SerializeField] GameObject DropItemPrefab;
+    [SerializeField] GameObject DropHealItemPrefab;
     [SerializeField] [Range(0f,1f)] float DropChance = 1f;
+    [SerializeField] [Range(0f,1f)] float HealDropChance = 1f;
 
     private void OnDestroy()
     {
         if (Random.value < DropChance)
         {
             Transform t = Instantiate(DropItemPrefab).transform;
+            t.position = transform.position;
+        }
+        else if  (Random.value < HealDropChance)
+        {
+            Transform t = Instantiate(DropHealItemPrefab).transform;
             t.position = transform.position;
         }
     }
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -3,9 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    public GameObject WeaponPanel;
+    public GameObject PlayerwhipPrefab;
     public void StartGame()
     {
+        WeaponPanel.SetActive(true);
+    }
+
+    public void weapon1()
+    {
         SceneManager.LoadScene("GameplayScene");
-        Time.timeScale = 1f;
+        Transform SpawnPos = GameObject.Find("SpawnPos").transform;
+        Instantiate(PlayerwhipPrefab, SpawnPos.position, SpawnPos.rotation);
     }
 }
